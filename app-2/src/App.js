779e51aa416e636a2e8a55fa538ddfe820c7c4ad
shawn's macbook <shawnscivally@shawn-ski.local>
2019-05-15 16:28:18 -0600

    problem 2 completed

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data = ['bike', 'jeep', 'tent', 'dog', 'skis']
+    }
+  }
   render() {
+    const packList = this.state.data.map((item, i) => {
+      return <h1 key={i}>{item}</h1>
+    })
+    
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {packList}
       </div>
     );
   }
